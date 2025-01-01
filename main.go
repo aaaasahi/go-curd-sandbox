a7aff35610648c48d806cc519d3ebe3834358b80
aaaasahi <asapjorf1017@gmail.com>
2025-01-01 13:43:41 +0900

    add basic http server

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
-	"io"
+	"go-crud-sandbox/handlers"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 func main() {
-helloHandler := func(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Hello, world!\n")
-}
-	http.HandleFunc("/", helloHandler)
+	r := mux.NewRouter()
+
+	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
+
+	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
+	r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
+
+	r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
+
 	log.Println("server start at port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
